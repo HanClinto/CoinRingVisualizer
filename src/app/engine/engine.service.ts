6880f2caa2bf1f5a56561e66913bdda901a14c3e
Clint Herron <cherron@apterainc.com>
2021-03-03 01:55:48 -0500

    Updating to display a coin.

--- src/app/engine/engine.service.ts
+++ src/app/engine/engine.service.ts
@@ -13,7 +13,11 @@ import {
   StandardMaterial,
   Texture,
   DynamicTexture,
-  Space
+  Space,
+  ArcRotateCamera,
+  Vector4,
+  MeshBuilder,
+  CSG
 } from '@babylonjs/core';
 
 @Injectable({ providedIn: 'root' })
@@ -31,52 +35,202 @@ export class EngineService {
     private windowRef: WindowRefService
   ) {}
 
+  /**
+   * Convert [x,y] cartesian coordinates to corresponding polar [x,y] coordinates
+   */
+  private static cartesianToPolar( x:number, y:number ) {
+    // iy = 0, always oy=0.5, ox=0.5
+    // iy = 1, oy=Math.sin(theta), ox=Math.cos(theta)
+    // theta = Math.atan2(x,y)
+  
+    var rho = y * 0.5;
+    var theta = x * 2 * Math.PI;
+  
+    var ox = 0.5 + Math.cos(theta) * rho;
+    var oy = 0.5 + Math.sin(theta) * rho;
+  
+    return {
+      x: ox,
+      y: oy
+    };
+  }
+
+  private static getPixel(x:number, y:number, imgData:ImageData) {
+    x = Math.floor(x);
+    y = Math.floor(y);
+    const i = 4 * (x + y * imgData.width);
+    if (x % 100 == 0 && y % 100 == 0) {
+      console.log(`4 * (${x} + ${y} * ${imgData.width}) => ${i}`);
+    }
+    return {
+      r:imgData.data[i + 0], 
+      g:imgData.data[i + 1], 
+      b:imgData.data[i + 2],
+      a:imgData.data[i + 3]
+    };
+  }
+
+  private static setPixel(x:number, y:number, imgData:ImageData, r:number, g:number, b:number, a:number) {
+    x = Math.floor(x);
+    y = Math.floor(y);
+    const i = 4 * (x + y * imgData.width);
+    imgData.data[i + 0] = r;
+    imgData.data[i + 1] = g;
+    imgData.data[i + 2] = b;
+    imgData.data[i + 3] = a;
+  }
+
   public createScene(canvas: ElementRef<HTMLCanvasElement>): void {
+    // Morgan back: https://en.numista.com/catalogue/photos/etats-unis/5ee614ca4780f9.05253990-original.jpg
+    // Morgan front: https://en.numista.com/catalogue/photos/etats-unis/5ee614c99912c4.90093382-original.jpg
     // The first step is to get the reference of the canvas element from our HTML document
+    // create a basic BJS Scene object
     this.canvas = canvas.nativeElement;
 
     // Then, load the Babylon 3D engine:
     this.engine = new Engine(this.canvas,  true);
-
-    // create a basic BJS Scene object
     this.scene = new Scene(this.engine);
     this.scene.clearColor = new Color4(0, 0, 0, 0);
+    const camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2.5, 4, Vector3.Zero(), this.scene);
+    camera.attachControl(this.canvas, true);
+    const light = new HemisphericLight("light", new Vector3(1, 1, 0), this.scene);
 
-    // create a FreeCamera, and set its position to (x:5, y:10, z:-20 )
-    this.camera = new FreeCamera('camera1', new Vector3(5, 10, -20), this.scene);
+    //Create dynamic texture
+    const DTWidth = 1024;
+    const DTHeight = 512;
 
-    // target the camera to scene origin
-    this.camera.setTarget(Vector3.Zero());
+    var textureInput;
+    var textureOutput = new DynamicTexture("dynamic texture", {width:DTWidth, height:DTHeight}, this.scene, true);
+    var outputContext = textureOutput.getContext();
 
-    // attach the camera to the canvas
-    this.camera.attachControl(this.canvas, false);
+    var materialOutput = new StandardMaterial("Mat", this.scene);
+    materialOutput.diffuseTexture = textureOutput;	
+    
+    var materialInput = new StandardMaterial("MatInput", this.scene);
 
-    // create a basic light, aiming 0,1,0 - meaning, to the sky
-    this.light = new HemisphericLight('light1', new Vector3(0, 1, 0), this.scene);
+    var scene = this.scene;
+    var img = new Image();
+    img.src = 'assets/textures/MorganDollarBack.jpg';
+    img.onload = function() {
+      console.log("Texture loaded.  Building custom image...");
+      let radius = img.width;
 
-    // create a built-in "sphere" shape; its constructor takes 4 params: name, subdivisions, radius, scene
-    this.sphere = Mesh.CreateSphere('sphere1', 16, 2, this.scene);
+      textureInput = new DynamicTexture("dynamic texture input", {width:img.width, height:img.height}, scene, false);
+      var inputContext = textureInput.getContext();
+      inputContext.drawImage(img, 0,0, img.width, img.height);
+      console.log(`Getting input imageData for ${img.width}, ${img.height}`);
+      let inData = inputContext.getImageData(0,0,img.width, img.height);
 
-    // create the material with its texture for the sphere and assign it to the sphere
-    const spherMaterial = new StandardMaterial('sun_surface', this.scene);
-    spherMaterial.diffuseTexture = new Texture('assets/textures/sun.jpg', this.scene);
-    this.sphere.material = spherMaterial;
+      textureInput.update();
+      materialInput.diffuseTexture = textureInput;
 
-    // move the sphere upward 1/2 of its height
-    this.sphere.position.y = 1;
+      //outputContext.drawImage(img, 0, 0, img.width, img.height, 0, 0, DTHeight, DTHeight);
 
-    // simple rotation along the y axis
-    this.scene.registerAfterRender(() => {
-      this.sphere.rotate (
-        new Vector3(0, 1, 0),
-        0.02,
-        Space.LOCAL
-      );
-    });
+      console.log("Creating output imageData");
+      let outData = outputContext.createImageData(DTWidth, DTHeight);
+      console.log(`Loaded imageData for ${inData.data.length} input and ${outData.data.length} output`);
+/*
+      for (let ii = 0; ii < inData.data.length; ii += 4000) {
+        console.log(`Pixel ${ii}: ${inData.data[ii+0]}, ${inData.data[ii+1]}, ${inData.data[ii+2]}, ${inData.data[ii+3]}`);
+      }
 
-    // generates the world x-y-z axis for better understanding
-    this.showWorldAxis(8);
-  }
+      for (let index = 0; index < outData.data.length; index += 4)
+      {
+        let i = index / 4;
+
+        let x = i % DTWidth;
+        let y = Math.floor(i / DTWidth);
+
+        var ix = x / DTWidth;
+        var iy = y / DTHeight;
+
+        let coords = EngineService.cartesianToPolar(ix, iy);
+
+        var ox = coords.x * img.width;
+        var oy = coords.y * img.height;
+
+        var inputindex = oy * 4 * img.width + ox * 4;
+
+        outData.data[index+0] = inData.data[inputindex+0];
+        outData.data[index+1] = inData.data[inputindex+1];
+        outData.data[index+2] = inData.data[inputindex+2];
+        outData.data[index+3] = 255; // inData.data[inputindex+3];
+      }
+
+      console.log("All done.  Writing back...");
+
+      outputContext.putImageData(outData, 0,0);
+      textureOutput.update();     
+      //*/     
+
+      //Add image to dynamic texture
+
+      const inc = 1;
+      console.log(`Input data is ${inData.width}x${inData.height}`);
+      for (let x = 0; x < DTWidth; x+= inc) {
+          for (let y = 0; y < DTHeight; y += inc) {
+              var ix = x / DTWidth;
+              var iy = y / DTHeight;
+              let coords = EngineService.cartesianToPolar(ix, iy);
+              
+              var srcPixel = EngineService.getPixel(coords.x * inData.width, coords.y * inData.height, inData);
+              if (x % 100 == 0 && y % 100 == 0) {
+                console.log(`Input (${ix},${iy}) = (${coords.x},${coords.y}) [(${coords.x* inData.width},${coords.y* inData.height})] : ${srcPixel.r}:${srcPixel.g}:${srcPixel.b}:${srcPixel.a}`);
+              }
+              EngineService.setPixel(x, y, outData, srcPixel.r, srcPixel.g, srcPixel.b, 255);
+              //EngineService.setPixel(x, y, outData, ix * 255, ix * 255, iy * 255, 255);
+
+              //outputContext.drawImage(img, 
+              //  coords.x * radius, coords.y * radius, inc, inc, 
+              //    x, y, inc, inc);
+          }
+      }
+      outputContext.putImageData(outData, 0,0);
+      textureOutput.update();
+      
+    }
+
+    const faceUV = [];
+    faceUV[0] =	new Vector4(0, 0, 0, 0);
+    faceUV[1] =	new Vector4(0, 0, 1, 0.75); // x, z swapped to flip image
+    faceUV[2] = new Vector4(0, 0, 1, 1);
+
+    this.scene.ambientColor = new Color3(0.3, 0.3, 0.3);
+  
+    var innerDiameter = 1.8;
+    var outerDiameter = 2;
+    var height = 1;
+    
+    var tess = 64;
+    
+    //var inner = BABYLON.Mesh.CreateCylinder("inner", height, innerDiameter, innerDiameter, tess, 1, scene);
+    var inner = MeshBuilder.CreateCylinder("inner", {height:height, diameterTop:innerDiameter, diameterBottom:innerDiameter, faceUV:faceUV, tessellation: tess, subdivisions: 1});
+  
+    //inner.material = canMaterial;
+    //var outer = BABYLON.MeshBuilder.CreateCylinder("outer", height, outerDiameter, outerDiameter, tess, 1, scene);
+    var outer = MeshBuilder.CreateCylinder("outer", {height:height, diameterTop:outerDiameter, diameterBottom:outerDiameter, faceUV:faceUV, tessellation: tess, subdivisions: 1});
+    outer.material = materialOutput;
+  
+    var innerCSG = CSG.FromMesh(inner);
+    var outerCSG = CSG.FromMesh(outer);
+    
+    var subCSG = outerCSG.subtract(innerCSG);
+    
+    var mat0 = new StandardMaterial("mat0", this.scene);
+  
+    var newMesh = subCSG.toMesh("csg2", materialOutput, this.scene);
+  
+    var plane = MeshBuilder.CreatePlane("plane", {size:1}, this.scene);
+    plane.material = materialInput;
+    plane.rotation.x = Math.PI * 0.5;
+    
+    this.scene.removeMesh(inner);
+    this.scene.removeMesh(outer);
+    
+    newMesh.rotation.y = Math.PI * 0.25;
+
+    //return this.scene;
+  };
 
   public animate(): void {
     // We have to run this outside angular zones,
